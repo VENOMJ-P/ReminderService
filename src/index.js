832498b211b247cf96e5a649a3f040bc22348aeb
VENOMJ-P <jay63837@gmail.com>
2023-08-27 12:35:52 +0530

    Fix some error in body-parser

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const {PORT} = require('./config/serverConfig')
 const setupAndStartServer = () => {
     const app = express();
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({urlencoded:true}));
+    app.use(bodyParser.urlencoded({extended:true}));
 
     app.listen(PORT, () => {
         console.log(`Server started at ${PORT}`);
