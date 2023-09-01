3744c67c9e14abb1ec36dfdd2484b9e053937f9a
VENOMJ-P <jay63837@gmail.com>
2023-09-01 20:14:48 +0530

    Added rabbit mq server queue

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ const {PORT} = require('./config/serverConfig');
 const {sendBaiscEmail} = require('./service/email-service')
 const jobs = require('./utils/jobs');
 const TicketContoller = require('./contoller/ticket-contoller');
+const {createChannel} = require('./utils/messageQueue')
 
-const setupAndStartServer = () => {
+const setupAndStartServer = async () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
+
+    const channel = await createChannel();
 
     app.post('/api/v1/tickets',TicketContoller.create)
     app.listen(PORT, () => {
